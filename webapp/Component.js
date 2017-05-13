68e37f40406b745ba7c5a76f0eb6eb1094c2f34f
ljs <sfayzullin86@gmail.com>
2017-05-13 19:15:22 +0000

    commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("demoproducts.comdemoproducts.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("demoproducts.comdemoproducts.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
